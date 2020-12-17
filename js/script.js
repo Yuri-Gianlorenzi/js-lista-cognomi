c835d772b3d27d5518575ac8dd64d05f1d3503ac
Yuri Gianlorenzi <yurigian@gmail.com>
2020-12-17 23:00:17 +0100

    end exercise, it works but no style I'm sorry

--- js/script.js
+++ js/script.js
@@ -10,19 +10,32 @@ var surnames = ['Gianlorenzi', 'biffi', 'Corsini', 'cannone', 'gorleta', 'alesia
 
 // qui chiediamo all'utente di inserire il proprio cognome tramite un prompt
 var userSurname = prompt('Inserisci il tuo cognome');
-console.log('cognome utente: ' + userSurname);
+
 
 // qui inseriamo il cognome dell'utente all'interno dell'Array
 surnames.push(userSurname);
-console.log(surnames);
+
 
 
 // qui creiamo un ciclo che trasforma i cognomi presenti e quello inserito dall'utente in maniera che si riesca ad ordinarli in ordine alfabetico, altrimenti se la lettera iniziale differiscono (minuscolo e maiuscolo), non riesce ad ordinarli
 for (var i = 0; i < surnames.length; i++) {
-  surnames[i] = surnames[i].toLowerCase();
+  surnames[i] = surnames[i].toUpperCase();
 }
 
 
 // con questo comando ordiniamo la lista di cognomi in ordine alfabetico
 surnames.sort();
-console.log('ordine alfabetico: ' + surnames);
+
+
+// qui viene usata un'altra variabile per creare un array identico, viene utilizzato join che da la possibilità di indicare il separatore tra gli elementi dell'array, nel nostro caso viene inserito <br> come separatore, per fare in modo che una volta stampato in html ogni successivo elemento dell'array va a capo
+var list = surnames.join('<br>');
+document.getElementById('list').innerHTML = list;
+
+
+// qui andiamo a cercare la posizione del cognome inserito dall'utente dopo che l'array è stato ordinato in ordine alfabetico. NB necessario inserire il touppercase quando specifichiamo in quale posizione, perchè il cognome inserito dall'utente è stato trasformato in maiuscolo
+var n = surnames.indexOf(userSurname.toUpperCase());
+
+
+// dato che le posizioni dell'array partono da 0, per conoscere la posizione reale aumentiamo di 1 la variabile che indica la posizione del cognome dell'utente nella lista
+var nReal = n + 1;
+document.getElementById('position').innerHTML = 'Lei si trova al ' + nReal + '&ordm; posto della lista';
